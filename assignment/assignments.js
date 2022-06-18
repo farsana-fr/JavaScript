f82eaf35d0847e6a0db4cf599e0d2da85e66b606
farsana-fr <104473273+farsana-fr@users.noreply.github.com>
2022-06-18 12:51:34 +0530

    Add files via upload
    Fundamental 1

---
+++ assignment/assignments.js
@@ -0,0 +1,31 @@
+
+console.log('assignment');
+console.log('=============');
+// 1
+const country="India";
+const continent ="Asia";
+let population =1380;
+console.log(country);
+console.log(continent);
+console.log(population);
+
+const isIsland=false;
+let language;
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
+language="Malayalam";
+console.log(typeof language);
+
+//4
+let halfPopulation=population/2;
+console.log(halfPopulation);
+population +=1;
+console.log(population);
+let populationOfFinland=6;
+console.log(population > populationOfFinland);
+let avg_Country=33;
+console.log(avg_Country < population);
+let description ="'"+country+" is in "+continent+", and its "+population+" million people speak "+language+"'";
+console.log(description);
